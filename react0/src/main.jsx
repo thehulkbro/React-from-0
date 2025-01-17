67b8fe03031d2291d5f505284080069b2c267750
thehulkbro <adas4504@gmail.com>
2025-01-17 15:36:47 +0530

    17.01.25

--- react0/src/main.jsx
+++ react0/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import App from './App'
+
+
+
+const ReactElement= React.createElement(
+  'a' ,
+  {href : 'https://www.google.com', target :'_blank'},
+  'click me'
+)
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {ReactElement} 
+    <App/> 
+    {/* Instead of using <ReactElement /> (which expects a component), use curly braces {ReactElement} to render the created React element. */}
   </StrictMode>,
 )
