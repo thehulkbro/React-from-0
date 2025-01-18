f01e195067ae830ad4513c0b8cd7a0081bb7c49d
thehulkbro <adas4504@gmail.com>
2025-01-18 15:35:07 +0530

    hooks basics

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # React-from-0
+
+
+//Day1
+learnt about creating custom react 
+behind the hood operations(basics)
+how react works
+
+//Day2
+what is hooks :
+            Hooks allow function components to have access to state and other React features. Because of this, class components are generally no longer needed.
+how to use it
+
+// Day3
--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,52 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 // HOOKS BASICS
 
 function App() {
-  
-  const[v , setV]=useState(11)
+  const [v, setV] = useState(11);
   // let  v = 11;
 
-  const clicked = () =>{
-    console.log('mr hulk' , Math.random())
+  const clicked = () => {
+    console.log("mr hulk", Math.random());
     //  v = v+1 ;
-    setV(v+1)
-    if(v>=20){
-      prompt("can't go beyond 20")
+    setV(v + 1);
+    if (v >= 20) {
+      prompt("can't go beyond 20");
       //setV(20)
     }
-  }
-  const clicked1 = () =>{
-    setV(v-1)
-    if(v<=0){
-      setV(0)
+  };
+  const clicked1 = () => {
+    setV(v - 1);
+    if (v <= 0) {
+      setV(0);
       console.error("can't go below 0");
-      
     }
-  }
+  };
 
   return (
     <>
-    <h1>ABHIJIT | HULK</h1>
-
-    <h3>The Value Is {v}</h3>
-    <button
-    on
-    onClick={clicked}                                   
-    >increase value</button>
-    <br/>
-    <br/>
-    <button
-    onClick={clicked1}
-    >decrease value</button>
-
-
+      <h1>ABHIJIT | HULK</h1>
+      <h3>The Value Is {v}</h3>
+      <button onClick={clicked}>increase value</button>
+      <br />
+      <br />
+      <button onClick={clicked1}>decrease value</button>
+      <br/>
+      <br/>
+      <button>testing</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
